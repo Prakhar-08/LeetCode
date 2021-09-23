c5f0fb433a3f9f4b096bc868d68e26efea8d7b43
Prakhar-08 <prakhar.1923it1011@kiet.edu>
2021-09-23 19:22:15 +0530

    Leetcode

--- Arrays/13. Trapping Rain Water.cpp
+++ Arrays/13. Trapping Rain Water.cpp
@@ -30,3 +30,35 @@ public:
         return sum;
     }
 };
+
+//Approach2 Optimal solution
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n<=2)
+            return 0;
+        
+        int l = 0, r = n-1, leftmax = 0, rightmax=0, sum=0;
+        
+        while(l < r) {
+            if(height[l] <= height[r]) {
+                if(height[l] >= leftmax)
+                    leftmax = height[l];
+                else
+                    sum += leftmax - height[l];
+                
+                l++;
+            }
+            else {
+                if(height[r] >= rightmax)
+                    rightmax = height[r];
+                else 
+                    sum += rightmax - height[r];
+                
+                r--;
+            }
+        }
+        return sum;
+    }
+};
