68ec72700e4642b7700f5df756f8a98a392b2f0b
Prakhar-08 <prakhar.1923it1011@kiet.edu>
2021-10-21 22:07:18 +0530

    Leetcode

---
+++ Arrays/21. Remove Element.cpp
@@ -0,0 +1,28 @@
+// Leetcode - 27
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+// Return k after placing the final result in the first k slots of nums.
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == val)
+                count++;
+        }
+        
+        sort(nums.begin(), nums.end());
+        int k = nums.size()-1;
+        for(int i=0; i<nums.size()-count; i++) {
+            if(nums[i] == val) {
+                swap(nums[i], nums[k]);
+                k--;
+            }
+        }
+        
+        return nums.size()-count;
+    }
+};
